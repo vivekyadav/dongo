c8c653a7335d63d11b219a0bb75b7896c2b18587
Vivek Yadav <vivek@vyadav.in>
2011-08-01 11:17:52 +0530

    added mongodb-engine

--- pyapp/wsgi.py
+++ pyapp/wsgi.py
@@ -3,6 +3,9 @@ import sys
 os.environ['DJANGO_SETTINGS_MODULE'] = 'djangomongo.settings'
 import django.core.handlers.wsgi
 
+proj_dir = os.path.abspath(os.path.dirname(__file__))
+sys.path.append(os.path.join(proj_dir, 'djangomongo'))
+
 djangoapplication = django.core.handlers.wsgi.WSGIHandler()
 
 def application(environ, start_response):
