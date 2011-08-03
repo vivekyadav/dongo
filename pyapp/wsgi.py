c1f8affc5a1d58c7391976967a0d17a8493d7a1e
Vivek Yadav <vivek@vyadav.in>
2011-08-03 14:17:56 +0530

    mongodb-engine

--- pyapp/wsgi.py
+++ pyapp/wsgi.py
@@ -6,7 +6,7 @@ import djangomongo.django.core.handlers.wsgi
 proj_dir = os.path.abspath(os.path.dirname(__file__))
 sys.path.append(os.path.join(proj_dir, 'djangomongo'))
 
-djangoapplication = django.core.handlers.wsgi.WSGIHandler()
+djangoapplication = djangomongo.django.core.handlers.wsgi.WSGIHandler()
 
 def application(environ, start_response):
     if 'SCRIPT_NAME' in environ:
